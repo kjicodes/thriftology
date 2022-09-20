250c3ae4a38ae6d8866f14a3932438a6c138c162
KI <kateleenissa@theMother.local>
2022-09-20 14:17:56 -0400

    Add listing details page

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,21 +5,13 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('accounts/signup/', views.signup, name='signup'),
-    path('listings/', views.listings_index, name="listings_index"),  # Kyle
-    path('mythrifts/', views.mythrifts_home,
-         name="mythrifts_index"),
-    path('mythrifts/listings', views.mythrifts_listings,
-         name='mythrifts_listings'),
-    path('mythrifts/sold', views.mythrifts_sold,
-         name='mythrifts_sold'),
-    path('mythrifts/bought', views.mythrifts_bought,
-         name='mythrifts_bought'),
-    path('listings/<int:listing_id>/add_photo/',
-         views.add_photo, name='add_photo'),
-
-    # path('listing/<int:listing_id>/',),  # Kateleen
+    path('listings/', views.listings_index, name="listings_index"),
+    path('listings/<int:listing_id>/', views.listings_detail, name='listings_detail'), #Kateleen - added 'details' path  
+    path('mythrifts/', views.mythrifts_home, name="mythrifts_index"),
+    path('mythrifts/listings', views.mythrifts_listings, name='mythrifts_listings'),
+    path('mythrifts/sold', views.mythrifts_sold, name='mythrifts_sold'),
+    path('mythrifts/bought', views.mythrifts_bought,name='mythrifts_bought'),
+    path('listings/<int:listing_id>/add_photo/', views.add_photo, name='add_photo'),
     # path('listing/<int:listing_id>/buy'),
-    path('listings/create/', views.CreateListing.as_view(),
-         name='create_listing'),  # Venessa
-
+    path('listings/create/', views.CreateListing.as_view(), name='create_listing')
 ]
